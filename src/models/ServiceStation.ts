5cda5a7974a5c2a08ce76f4487434e969f742930
Deneth05 <denethcharuka951@gmail.com>
2025-04-07 09:58:47 +0530

    Report

--- src/models/ServiceStation.ts
+++ src/models/ServiceStation.ts
@@ -1,22 +1,23 @@
-  // models/ServiceStation.ts
-  import mongoose from "mongoose";
+// models/ServiceStation.ts
+import mongoose from "mongoose";
 
-  const serviceStationSchema = new mongoose.Schema({
-    businessRegNo: { type: String, required: true },
-    businessName: { type: String, required: true },
-    branch: { type: String, required: false },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    postalCode: { type: String, required: true },
-    email: { type: String, required: true },
-    phoneNumber1: { type: String, required: true },
-    phoneNumber2: { type: String, required: false },
-    ownerName: { type: String, required: true },
-    contactNumber: { type: String, required: true },
-    email2: { type: String, required: false },
-    webUrl: { type: String, required: false },
-  });
+const serviceStationSchema = new mongoose.Schema({
+  businessRegNo: { type: String, required: true },
+  businessName: { type: String, required: true },
+  businessType: { type: String, required: true }, // Added businessType field
+  branch: { type: String, required: false },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNumber1: { type: String, required: true },
+  phoneNumber2: { type: String, required: false },
+  ownerName: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+  email2: { type: String, required: false },
+  webUrl: { type: String, required: false },
+});
 
-  const ServiceStation = mongoose.model("ServiceStation", serviceStationSchema);
+const ServiceStation = mongoose.model("ServiceStation", serviceStationSchema);
 
-  export default ServiceStation;
+export default ServiceStation;
